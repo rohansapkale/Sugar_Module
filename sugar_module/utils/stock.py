657ebe6ab1ee26643090d4745061248452ea6d84
Rohan <frappe@WD1571520396noidaspeedvpsc9CZuAduwy.test.com>
2026-07-30 12:11:39 +0100

    Dispatch History bug  resolved

--- sugar_module/utils/stock.py
+++ sugar_module/utils/stock.py
@@ -1,6 +1,72 @@
 import frappe
 
 
+def refresh_dispatch_history(purchase_name):
+    """
+    Rebuild Dispatch History table inside Sugar Purchase
+    """
+
+    purchase = frappe.get_doc("Sugar Purchase", purchase_name)
+
+    # Clear existing rows
+    purchase.set("dispatch_history", [])
+
+    # Get all submitted dispatch entries for this purchase
+    dispatches = frappe.get_all(
+        "Dispatch Entry",
+        filters={
+            "sugar_purchase": purchase_name,
+            "docstatus": 1
+        },
+        fields=[
+            "name",
+            "dispatch_date",
+            "broker",
+            "vehicle_no",
+            "customer_name",
+            "dispatch_qty_quintal",
+            "rate",
+            "total_amount",
+            "paid_amount",
+            "balance_amount",
+            "payment_status",
+            "status"
+        ],
+        order_by="dispatch_date asc",
+        as_list=False
+    )
+
+    # Populate child table
+    for d in dispatches:
+
+        broker_name = frappe.db.get_value(
+            "Broker",
+            d.broker,
+            "broker_name"
+        )
+
+
+        purchase.append("dispatch_history", {
+            "dispatch_date": d.dispatch_date,
+            "broker": broker_name,
+            "customer": d.customer_name,
+            "vehicle_no":d.vehicle_no,
+            "dispatch_qty_quintal": d.dispatch_qty_quintal,
+            "rate": d.rate,
+            "total_amount": d.total_amount,
+            "paid_amount": d.paid_amount,
+            "balance_amount": d.balance_amount,
+            "payment_status": d.payment_status,
+            "status": d.status
+        })
+
+    purchase.flags.ignore_validate_update_after_submit = True
+
+    purchase.save(
+        ignore_permissions=True,
+        ignore_version=True
+    )
+
 def get_total_purchased():
     result = frappe.db.sql("""
         SELECT IFNULL(SUM(converted_qty_kg), 0)
